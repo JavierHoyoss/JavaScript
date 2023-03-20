992b4747d7b05abb68775bd158fc3a1bba95d580
JavierHoyoss <sebastianhoyos12@gmail.com>
2023-03-19 22:47:32 -0300

    sync await completo

--- scripts/details.js
+++ scripts/details.js
@@ -1,26 +1,45 @@
-const arrayEvent = data.events;
+let urlAPI = "https://mindhub-xj03.onrender.com/api/amazing";
 
-const queryString = location.search;
+async function getData() {
+    try {
+        let respuesta = await fetch(urlAPI)
+        console.log(respuesta)
+        let datos = await respuesta.json()
+        console.log(datos.events)
 
-const params = new URLSearchParams(queryString);
+        const arrayEvent = datos.events;
 
-const id = params.get("id");
+        const queryString = location.search;
 
-const evento=arrayEvent.find(even=>even._id==id);
+        const params = new URLSearchParams(queryString);
 
-console.log(evento)
+        const id = params.get("id");
 
-const details = document.querySelector(".carddet");
-const template = document.querySelector(".carddet").content;
+        const evento = arrayEvent.find(even => even._id == id);
 
+        console.log(evento)
 
-template.querySelector("img").src=evento.image;
-template.querySelector("figcaption").textContent=evento.name;
-template.querySelector("h2").textContent=evento.name;
-template.querySelector("p").textContent=evento.description;
+        const details = document.querySelector(".carddet");
+        const template = document.querySelector(".carddet").content;
 
 
+        template.querySelector("img").src = evento.image;
+        template.querySelector("figcaption").textContent = evento.name;
+        template.querySelector("h2").textContent = evento.name;
+        template.querySelector("p").textContent = evento.description;
 
-const clone = template.cloneNode(true);
 
-details.appendChild(clone);
+
+        const clone = template.cloneNode(true);
+
+        details.appendChild(clone);
+
+    } catch {
+        console.log('ocurrio un error con mi api')
+    }
+}
+
+getData()
+
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,107 +1,124 @@
 
-
-const tarjetas = document.querySelector(".tarjetas");
-const arrayEvent = data.events;
-const fragment = document.createDocumentFragment();
+let urlAPI = "https://mindhub-xj03.onrender.com/api/amazing";
 
 
-
-let categorias = [];
-arrayEvent.forEach((item) => {
-  categorias.push(item.category)
-})
-//filtro las categorias para usar el template
-const catFiltrada = categorias.filter((valor, indice) => {
-  return categorias.indexOf(valor) === indice;
-}
-);
-
-//template de las categorias
-const checkboxes = document.querySelector(".checkboxes");
-const template = document.querySelector(".checkbox").content;
-
-catFiltrada.forEach((item) => {
-  template.querySelector("label").textContent = item;
-  template.querySelector("label").setAttribute("for", item);
-  template.querySelector("input").id = item;
+async function getData() {
+  try {
+    let respuesta = await fetch(urlAPI)
+    console.log(respuesta)
+    let datos = await respuesta.json()
+    console.log(datos.events)
+    const tarjetas = document.querySelector(".tarjetas");
+    const arrayEvent = datos.events;
+    const fragment = document.createDocumentFragment();
 
 
-  const clone = template.cloneNode(true);
-  fragment.appendChild(clone);
-})
-checkboxes.appendChild(fragment);
 
-
-//filtrado de eventos
-const search = document.getElementById("search")
-const btn_search = document.getElementById("btn_busqueda")
-const checks = document.querySelectorAll(".check");
-
-const filtrar = () => {
-  const selectedCategories = Array.from(checks)
-    .filter((checkbox) => checkbox.checked)
-    .map((checkbox) => checkbox.id);
-  // Filtrar los eventos
-  const filteredEvents = arrayEvent.filter(
-    (event) =>
-      (selectedCategories.length === 0 ||
-        selectedCategories.includes(event.category))
-  );
-  tarjetas.innerHTML = ''; 
-  const texto = search.value.toLowerCase();
-  //filtrado por input de busqueda
-  filteredEvents.forEach((item) => {
-    let nombre = item.name.toLowerCase();
-    let descripcion = item.description.toLowerCase();
-    if (nombre.indexOf(texto) !== -1 || descripcion.indexOf(texto) !== -1) {
-      const tarjeta = document.createElement("div");
-      tarjeta.className = "tarjeta";
-      const imgDiv = document.createElement("div");
-      imgDiv.className = "cardimg";
-      const img = document.createElement("img");
-      img.src = item.image;
-      img.alt = item.category;
-      const card = document.createElement("div");
-      card.className = "card";
-      const cardtitulo = document.createElement("div");
-      cardtitulo.textContent = item.name;
-      cardtitulo.className = "cardtitulo";
-      const cardtxt = document.createElement("div");
-      cardtxt.className = "cardtxt";
-      const p = document.createElement("p");
-      p.textContent = item.description;
-      const p2 = document.createElement("p");
-      p2.innerHTML = "<b>Price:</b> $" + item.price + "<br> <b>Place: </b>" + item.place;
-      const link = document.createElement("a");
-      link.href = `./details.html?id=${item._id}`;
-      link.textContent = "Details";
-      link.dataset.id = item._id;
-
-
-      tarjeta.appendChild(imgDiv);
-      imgDiv.appendChild(img);
-      tarjeta.appendChild(card);
-      card.appendChild(cardtitulo);
-      card.appendChild(cardtxt);
-      cardtxt.appendChild(p);
-      cardtxt.appendChild(p2);
-      cardtxt.appendChild(link);
-
-      fragment.appendChild(tarjeta);
+    let categorias = [];
+    arrayEvent.forEach((item) => {
+      categorias.push(item.category)
+    })
+    //filtro las categorias para usar el template
+    const catFiltrada = categorias.filter((valor, indice) => {
+      return categorias.indexOf(valor) === indice;
     }
-    tarjetas.appendChild(fragment);
-  })
-  if (tarjetas.innerHTML === '') {
-    tarjetas.innerHTML += "No hay resultados, por favor modificar los filtros"
+    );
+
+    //template de las categorias
+    const checkboxes = document.querySelector(".checkboxes");
+    const template = document.querySelector(".checkbox").content;
+
+    catFiltrada.forEach((item) => {
+      template.querySelector("label").textContent = item;
+      template.querySelector("label").setAttribute("for", item);
+      template.querySelector("input").id = item;
+
+
+      const clone = template.cloneNode(true);
+      fragment.appendChild(clone);
+    })
+    checkboxes.appendChild(fragment);
+
+
+    //filtrado de eventos
+    const search = document.getElementById("search")
+    const btn_search = document.getElementById("btn_busqueda")
+    const checks = document.querySelectorAll(".check");
+
+    const filtrar = () => {
+      const selectedCategories = Array.from(checks)
+        .filter((checkbox) => checkbox.checked)
+        .map((checkbox) => checkbox.id);
+      // Filtrar los eventos
+      const filteredEvents = arrayEvent.filter(
+        (event) =>
+        (selectedCategories.length === 0 ||
+          selectedCategories.includes(event.category))
+      );
+      tarjetas.innerHTML = '';
+      const texto = search.value.toLowerCase();
+      //filtrado por input de busqueda
+      filteredEvents.forEach((item) => {
+        let nombre = item.name.toLowerCase();
+        let descripcion = item.description.toLowerCase();
+        if (nombre.indexOf(texto) !== -1 || descripcion.indexOf(texto) !== -1) {
+          const tarjeta = document.createElement("div");
+          tarjeta.className = "tarjeta";
+          const imgDiv = document.createElement("div");
+          imgDiv.className = "cardimg";
+          const img = document.createElement("img");
+          img.src = item.image;
+          img.alt = item.category;
+          const card = document.createElement("div");
+          card.className = "card";
+          const cardtitulo = document.createElement("div");
+          cardtitulo.textContent = item.name;
+          cardtitulo.className = "cardtitulo";
+          const cardtxt = document.createElement("div");
+          cardtxt.className = "cardtxt";
+          const p = document.createElement("p");
+          p.textContent = item.description;
+          const p2 = document.createElement("p");
+          p2.innerHTML = "<b>Price:</b> $" + item.price + "<br> <b>Place: </b>" + item.place;
+          const link = document.createElement("a");
+          link.href = `./details.html?id=${item._id}`;
+          link.textContent = "Details";
+          link.dataset.id = item._id;
+
+
+          tarjeta.appendChild(imgDiv);
+          imgDiv.appendChild(img);
+          tarjeta.appendChild(card);
+          card.appendChild(cardtitulo);
+          card.appendChild(cardtxt);
+          cardtxt.appendChild(p);
+          cardtxt.appendChild(p2);
+          cardtxt.appendChild(link);
+
+          fragment.appendChild(tarjeta);
+        }
+        tarjetas.appendChild(fragment);
+      })
+      if (tarjetas.innerHTML === '') {
+        tarjetas.innerHTML += "No hay resultados, por favor modificar los filtros"
+      }
+    }
+
+
+    search.addEventListener('keyup', filtrar)
+    //verificar chech del checkbox
+    checks.forEach((checkbox) => checkbox.addEventListener("change", filtrar));
+
+    filtrar();
+
+
+  } catch {
+    console.log('ocurrio un error con mi api')
   }
-} 
+}
 
+getData()
 
-search.addEventListener('keyup', filtrar)
-//verificar chech del checkbox
-checks.forEach((checkbox) =>checkbox.addEventListener("change", filtrar));
-
-filtrar();
 
 
 
